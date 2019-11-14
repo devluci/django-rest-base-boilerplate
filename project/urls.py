a1a23a6a144810b31e5217fb4babe4804caf0894
Lucid <devluci@users.noreply.github.com>
2019-11-14 11:46:32 +0900

    apply ADMIN_PREFIX

--- project/urls.py
+++ project/urls.py
@@ -2,8 +2,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 
+if settings.ADMIN_PREFIX:
+    admin_endpoint = settings.ADMIN_PREFIX + '/admin/'
+else:
+    admin_endpoint = 'admin/'
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(admin_endpoint, admin.site.urls),
 ]
 
 for app in settings.APPS:
